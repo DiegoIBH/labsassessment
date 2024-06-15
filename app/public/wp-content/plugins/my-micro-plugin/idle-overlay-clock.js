e9b2a830e0c941b3b28c4ac5382ea3d0f830c6ac
DiegoIBH <diegobarrosh1163@gmail.com>
2024-06-15 16:15:59 +0200

    Update php and JS files, adding functionality(clock, reset triggers and update) in js and localize function + hook in php

--- app/public/wp-content/plugins/my-micro-plugin/idle-overlay-clock.js
+++ app/public/wp-content/plugins/my-micro-plugin/idle-overlay-clock.js
@@ -8,17 +8,40 @@ jQuery(document).ready(function($) {
         }
     }
 
+    function resetTimer() {
+        idleTime = 0;
+        if (idleTime < 8) {
+            hideOverlay();
+        }
+    }
+
     function showOverlay() {
         $('#ioc-overlay').fadeIn();
-        
+        updateClock();
         
     }
+
+    function hideOverlay() {
+        $('#ioc-overlay').fadeOut();
+    }
+
+    function updateClock() {
+        let now = new Date();
+        $('#ioc-clock').text(now.toLocaleTimeString());
+        $('#ioc-date').text(now.toDateString());
+    }
+
     setInterval(timerIncrement, 1000);
+    setInterval(updateClock, 1000);
+
+    //Reset trigger
+    $(this).mousemove(resetTimer);
+    $(this).keypress(resetTimer);
 
     //append the html code in body
     $('body').append(`
         <div id="ioc-overlay" style="display:none;">
-            <div id="ioc-clock">Clock</div>
+            <div id="ioc-clock"></div>
             <div id="ioc-date"></div>
             <div id="ioc-user-info">
                 <ul>
--- app/public/wp-content/plugins/my-micro-plugin/idle-overlay-clock.php
+++ app/public/wp-content/plugins/my-micro-plugin/idle-overlay-clock.php
@@ -12,5 +12,15 @@ function ioc_enqueue_scripts() {
     
 }
 
-add_action('wp_enqueue_scripts', 'ioc_enqueue_scripts'); //Hook to the wp acction that calls our fucntion.
+add_action('wp_enqueue_scripts', 'ioc_enqueue_scripts'); //Hook to the wp acction that calls our function.
 
+function ioc_localize_script() {
+    $current_user = wp_get_current_user();// Instantiate the current user through the wp_get_current_user wp method.
+    wp_localize_script('ioc-custom-script', 'ioc_user_data', array( // Pass the current user data to the script as an object, with the values contained in the array.
+        'is_logged_in' => is_user_logged_in(), // request user status with the is_user_logged_in() method
+        'user_name' => $current_user->display_name, // request user name with the display_name property
+        'user_email' => $current_user->user_email   // request user email with the user_email property
+    ));
+}
+
+add_action('wp_enqueue_scripts', 'ioc_localize_script');//Hook
